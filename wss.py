c0669d6d0163d751e72e2c81b953f51a11fa0733
belial1337 <b3l14l@gmail.com>
2014-06-24 15:09:50 +0100

    Added threading

--- wss.py
+++ wss.py
@@ -4,10 +4,11 @@ import re
 import binascii
 import struct
 import time
+import sys
 from random import randint
 from base64 import b64encode
 from hashlib import sha1
-
+from thread import *
 
 events = "/var/www/map/eventstream"
 
@@ -28,32 +29,78 @@ websocket_answer = (
 GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.1.101', 443))
+print 'Socket Created'
+try:
+	s.bind(('192.168.1.101', 443))
+except socket.error as msg:
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+print 'Socket bind complete'
 s.listen(10)
 print "Listening for connections"
- 
-while True:
+
+def clientthread(client):
+	print 'main function'
+	while True:	
+		for line in content:
+			print 'Function 1'
+			length = len(line)
+                       	preamble = "\x81\x7e" + struct.pack(">i", length)[2:]
+                        client.send(preamble+line)
+                        print "Sending Attack Event Size: " + hex(length) + " Bytes\n"
+                        time.sleep(randint(0,3))
+		client.close()
+
+
+while 1:
 	client, address = s.accept()
 	print 'Got connection from', address
 	text = client.recv(1024)
 	print text
- 
-	key = (re.search('Sec-WebSocket-Key:\s+(.*?)[\n\r]+', text)
-   	.groups()[0]
-   	.strip())
- 
-	response_key = b64encode(sha1(key + GUID).digest())
-	response = '\r\n'.join(websocket_answer).format(key=response_key)
+  	key = (re.search('Sec-WebSocket-Key:\s+(.*?)[\n\r]+', text)
+        .groups()[0]
+        .strip())
 
-	print response 
+        response_key = b64encode(sha1(key + GUID).digest())
+        response = '\r\n'.join(websocket_answer).format(key=response_key)
+		
+	print response
 	client.send(response)
 	client.recv(1)
+	start_new_thread(clientthread ,(client,))
+s.close()
+#		s.close()
+#		for line in content:
+#			length = len(line)
+#			preamble = "\x81\x7e" + struct.pack(">i", length)[2:]
+#			client.send(preamble+line)
+#			print "Sending Attack Event Size: " + hex(length) + " Bytes\n"
+#			time.sleep(randint(0,3))
+#		start_new_thread(clientthread ,(client,))
+#	client.close()
+
+ 
+#while 1:
+#	client, address = s.accept()
+#	print 'Got connection from', address
+#	text = client.recv(1024)
+#	print text
+
+#	key = (re.search('Sec-WebSocket-Key:\s+(.*?)[\n\r]+', text)
+#   	.groups()[0]
+#   	.strip())
+ 
+#	response_key = b64encode(sha1(key + GUID).digest())
+#	response = '\r\n'.join(websocket_answer).format(key=response_key)
+
+#	print response 
+#	client.send(response)
+#	client.recv(1)
 
 
-	for line in content:
-		length = len(line)
-		preamble = "\x81\x7e" + struct.pack(">i", length)[2:]
-		client.send(preamble+line)
-		print "Sending Attack Event Size: " + hex(length) + " Bytes\n"
-		time.sleep(randint(0,3))		
-		s.close()
+#for line in content:
+#length = len(line)
+#preamble = "\x81\x7e" + struct.pack(">i", length)[2:]
+#client.send(preamble+line)
+#print "Sending Attack Event Size: " + hex(length) + " Bytes\n"
+#time.sleep(randint(0,3))		
